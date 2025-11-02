a84ffe20b13e1df63876bc86895e95321db272b3
accio3014 <accio3014@gmail.com>
2025-11-02 18:12:03 +0900

    Update minecraft python code

--- python/minecraft_python/sources/string_min.py
+++ python/minecraft_python/sources/string_min.py
@@ -1,1 +1,45 @@
-from minecraft import *
+# from minecraft import *
+
+# chat()
+# chat("문자")의 경우 문자를 마인크래프트에 출력함, 문자가 저장된 변수도 사용 가능
+# print()와 다르게 콤마(,)로 여러개를 출력하지 못함
+
+# chat("Hello welcome!")
+
+# say = "nice to meet you!"
+# chat(say)   # 문자가 저장된 변수 사용
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# 내가 입력한 것을 마인크래프트에 출력
+# say = input()
+# chat(say)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# say = "Hello, welcome!"
+# chat(say)
+
+# say = input()
+# chat(say)
+
+# say = input("say again in python : ")
+# chat(say)
+
+# saycon = input("we concatenate text : ")
+# chat(say + " " + saycon)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# name = input("what is your name? : ")
+
+# say = "your name is " + name
+# chat(name)
+# chat(say)
+# print(say)
