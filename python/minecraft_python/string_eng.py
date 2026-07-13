e0ce0cbf7ffc046565a25afb1cc0a8786d6b6e86
acciopi <accio3014@gmail.com>
2026-07-13 13:57:24 +0900

    Minecraft python english ver update

---
+++ python/minecraft_python/string_eng.py
@@ -0,0 +1,94 @@
+import time     # Import time tools
+
+
+# time.sleep(number)
+# Stops the program for the given number of seconds.
+# It is not used very often in real programs.
+
+# print('1')
+# time.sleep(1)   # Print 1, then wait for 1 second.
+# print('2')
+# time.sleep(2)   # Print 2, then wait for 2 seconds.
+# print('3')
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Strings
+# A string is one type of data.
+# A data type tells us what kind of value it is.
+# Text inside quotation marks is always a string.
+# You can use double quotes " " or single quotes ' '.
+
+# name = "jack"
+# where = "cit"
+# a = '5'         # This is the string "5", not the number 5.
+
+# print(name)
+# print(where)
+# print(name, where, a)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# input('text' or variable)
+# It shows text and waits for the user to type something and press Enter.
+# The text or variable can be left out.
+# variable = input('text')
+# This is the most common way to use input().
+# If you use only input(), the entered value is not saved.
+# input() always saves the value as a str type.
+
+# name = input()
+# print(name)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Adding strings
+# You can join strings using the + operator.
+
+# first = "cit"
+# second = "academy"
+
+# name1 = "cit" + " " + "academy"
+# print(name1)
+
+# name2 = "cit" + " " + second
+# print(name2)
+
+# name3 = first + " " + second
+# print(name3)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# Type casting
+# str(variable or value)     -> Changes it to a string.
+# float(variable or value)   -> Changes it to a float.
+# int(variable or value)     -> Changes it to an integer.
+
+# If you use type casting only in a calculation,
+# the original variable does not change.
+
+# To change the original variable's data type,
+# save the changed value back into the variable.
+# Example: a = int(a)
+
+# one = "1"
+# two = "2"
+
+# The strings in one and two are changed to integers.
+# three = int(one) + int(two)
+# print(three)
+
+# four = 4
+# zero = 0
+
+# The numbers in four and zero are changed to strings.
+# forty = str(four) + str(zero)
+# print(forty)
